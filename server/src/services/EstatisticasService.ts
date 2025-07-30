17aebeba0cbf312becc4fba0b30b68aff907e1e4
gabrieloftas <gabrieloftas@outlook.com>
2025-07-30 15:59:46 -0300

    Adicao de estatisticas

---
+++ server/src/services/EstatisticasService.ts
@@ -0,0 +1,41 @@
+import { any } from "zod";
+import prisma from "../config/prismaClient";
+
+export class EstatisticasService {
+  public async getQuantidadeProfissionais(): Promise<number> {
+    return prisma.usuarios.count();
+  }
+
+  public async getQuantidadePacientes(): Promise<number> {
+    return prisma.pacientes.count();
+  }
+
+  public async getMediaAvaliacoes(): Promise<number> {
+    const result = await prisma.agendamentos.aggregate({
+      _avg: {
+        nota_atendimento: true
+      },
+      where: {
+        nota_atendimento: {
+          not: null
+        }
+      }
+    });
+
+    return result._avg.nota_atendimento || 0;
+  }
+
+  public async getEstatisticasHome() {
+    const [quantidadeProfissionais, quantidadePacientes, mediaAvaliacoes] = await Promise.all([
+      this.getQuantidadeProfissionais(),
+      this.getQuantidadePacientes(),
+      this.getMediaAvaliacoes()
+    ]);
+
+    return {
+      quantidadeProfissionais,
+      quantidadePacientes,
+      mediaAvaliacoes: Number(mediaAvaliacoes.toFixed(1))
+    };
+  }
+}
